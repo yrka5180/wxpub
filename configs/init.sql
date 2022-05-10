ddb8db5baec7f7020e00ca8397bcb3c3d2bcfbd7
leeoj2 <329981504@qq.com>
2022-05-10 18:00:16 +0800

    init

--- configs/init.sql
+++ configs/init.sql
@@ -27,10 +27,10 @@ CREATE TABLE `user`  (
 DROP TABLE IF EXISTS `failure_msg_log`;
 CREATE TABLE `failure_msg_log`  (
                          `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键id',
-                         `msg_id` int(11) NOT NULL DEFAULT 0 COMMENT '消息id',
+                         `msg_id` bigint(64) NOT NULL DEFAULT 0 COMMENT '消息id',
                          `to_user` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '接收者openid',
                          `template_id` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '模板id',
-                         `content` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '模板内容',
+                         `content` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '模板内容',
                          `cause` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '失败原因',
                          `status` tinyint(1) NOT NULL DEFAULT 0 COMMENT '发送状态，1为正常，2为重试中，3为失败',
                          `count` tinyint(1) NOT NULL DEFAULT 0 COMMENT '发送次数',
