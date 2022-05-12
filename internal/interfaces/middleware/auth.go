ee9c86b5716fc4a8e05978390469f9a44cf7dc96
leeoj2 <329981504@qq.com>
2022-05-12 11:39:58 +0800

    init

--- internal/interfaces/middleware/auth.go
+++ internal/interfaces/middleware/auth.go
@@ -1,7 +1,7 @@
 package middleware
 
 import (
-	"fmt"
+	"git.nova.net.cn/nova/misc/wx-public/proxy/internal/interfaces/httputil"
 
 	"git.nova.net.cn/nova/misc/wx-public/proxy/internal/application"
 	"git.nova.net.cn/nova/misc/wx-public/proxy/internal/consts"
@@ -27,21 +27,14 @@ func (p *Passport) VerifyToken(c *gin.Context) {
 	auth := c.GetHeader(consts.Authorization)
 	if len(auth) <= 0 {
 		log.Errorf("verifyToken oidc introspect failed,traceID:%s", traceID)
-		p.abort(c, errors.CodeUnauthorized, errors.GetErrorMessage(errors.CodeUnauthorized), traceID)
+		httputil.Abort(c, errors.CodeUnauthorized, errors.GetErrorMessage(errors.CodeUnauthorized), traceID)
 		return
 	}
 	err := p.passportApp.GetAuthN(ctx, auth)
 	if err != nil {
 		log.Errorf("VerifyToken GetAuthN failed,traceID:%s,err:%v", traceID, err)
-		p.abort(c, err.(errors.CustomError).ErrorCode, err.(errors.CustomError).ErrorMsg, traceID)
+		httputil.Abort(c, err.(errors.CustomError).ErrorCode, err.(errors.CustomError).ErrorMsg, traceID)
 		return
 	}
 	c.Next()
-}
-
-func (p *Passport) abort(ctx *gin.Context, errCode int, errMsg string, traceID string) {
-	ctx.Header(errors.XCode, fmt.Sprintf("%d", errCode))
-	ctx.Header(errors.XMsg, errMsg)
-	ctx.Header(errors.XTraceID, traceID)
-	ctx.AbortWithStatus(errors.GetStatusCode(errCode))
 }
